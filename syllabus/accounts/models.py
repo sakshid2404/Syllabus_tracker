fc9e7482d642ef1085833f62c949455f5c294034
sakshi <sdesale2004@gmail.com>
2025-08-12 21:51:59 +0530

    done

--- syllabus/accounts/models.py
+++ syllabus/accounts/models.py
@@ -7,7 +7,7 @@ class User(AbstractUser):
     email=models.EmailField(unique=True)
     password=models.CharField(max_length=10)
     created_at = models.DateTimeField(auto_now_add=True)
-
+    
     def __str__(self):
         return self.username
 
--- syllabus/app/models.py
+++ syllabus/app/models.py
@@ -7,12 +7,4 @@ class Syllabus(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-<<<<<<< HEAD
-        return self.name 
-=======
         return self.name
-<<<<<<< HEAD
-=======
->>>>>>> 6a2beae6b079b7ab8e26ec8bcd9888130852c84b
->>>>>>> 5c90ba54847eff0b933159f165cdc1c1e6a698f2
->>>>>>> 230f2e019ee2793dd150dfa0840030729ed733a1
