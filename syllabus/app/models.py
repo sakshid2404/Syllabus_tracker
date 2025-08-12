6fc6c7bfb4f6e9456de8ac1d25b7bb8639ec9edd
jayashri <jaysharipatil01@gmail.com>
2025-08-12 17:46:26 +0530

    initt

--- syllabus/app/models.py
+++ syllabus/app/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
-
 class Syllabus(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
